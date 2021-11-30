7ed4de75a6e7ce8414f5a1ad50c82f21268cce30
Itsnotjustnate <nathaneduvala@csu.fullerton.edu>
2021-11-29 17:51:57 -0800

    resources file

--- game.py
+++ game.py
@@ -9,3 +9,13 @@ class Game:
 
     def run(self):
         pygame.init()
+
+        running = True
+
+        while running:
+            for event in pygame.event.get():
+                if event.type == KEYDOWN:
+                    if event.key == K_ESCAPE:
+                        running = False
+                elif event.type == QUIT:
+                    running = False
